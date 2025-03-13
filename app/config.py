a534f1b8c638002cab2982e191a0fd95712d412b
Fateh Khan <fatehkhann@gmail.com>
2025-03-13 20:43:08 +0500

    added new env variables

--- app/config.py
+++ app/config.py
@@ -3,5 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     groq_api_key: str
     database_url: str
+    username: str
+    password: str
+    host: str 
+    database: str
 
     model_config = SettingsConfigDict(env_file=".env")
