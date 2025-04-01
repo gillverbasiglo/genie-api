cdecdd5fd95c82c7c6ae6958e5d2ee0802c0e83e
gillverbasiglo <dev.coder39@gmail.com>
2025-03-31 21:20:47 -0400

    Fix database secret extraction
    Signed-off-by: Carlos Aguilar <caguilar@dwdandsolutions.com>

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 # PostgreSQL connection string
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.host}:{settings.port}/{settings.database}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password.get_secret_value()}@{settings.host}:{settings.port}/{settings.database}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
