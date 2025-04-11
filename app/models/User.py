f2497d9ed08fade2c4ace4fdb0fa44513f44ff7f
gillverbasiglo <dev.coder39@gmail.com>
2025-04-10 20:53:01 -0400

    Added initial migration
    This added the initial migration but also add the keywords and archetypes fields for User model
    
    Signed-off-by: Carlos Aguilar <caguilar@dwdandsolutions.com>

--- app/models/User.py
+++ app/models/User.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy import Column, String, DateTime, ForeignKey, ARRAY
 from sqlalchemy.orm import relationship
 from ..database import Base
 from datetime import datetime, timezone
@@ -12,6 +12,8 @@ class User(Base):
     display_name = Column(String, nullable=True)
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
     invited_by = Column(String, ForeignKey("users.id"), nullable=True)
+    archetypes = Column(ARRAY(String), nullable=True)
+    keywords = Column(ARRAY(String), nullable=True)
     
     # Relationships
     sent_invites = relationship("Invitation", back_populates="inviter", foreign_keys="Invitation.inviter_id")
