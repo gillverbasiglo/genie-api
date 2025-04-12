fbbbe69762388e188648b560060442a899b99ffa
Fateh Khan <fatehkhann@gmail.com>
2025-04-12 15:59:34 +0500

    Removed unused import

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-from .database import *
 
 # PostgreSQL connection string
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password.get_secret_value()}@{settings.host}:{settings.port}/{settings.database}"
