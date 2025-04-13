e444dea4d5c60d0a2e349bbefd686c6a7736d475
Fateh Khan <fatehkhann@gmail.com>
2025-04-13 21:38:35 +0500

    Revert config

--- app/config.py
+++ app/config.py
@@ -17,14 +17,9 @@ class Settings(BaseSettings):
     google_api_key: SecretStr
     tavily_api_key: SecretStr
     exa_api_key: SecretStr
-    apns_use_sandbox: bool = True
-    apns_auth_key_path: str
-    apns_key_id: SecretStr
-    apns_team_id: SecretStr
-    apns_bundle_id: SecretStr
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-    @field_validator("db_username", "db_password", "groq_api_key", "openai_api_key", "trip_advisor_api_key", "google_api_key", "tavily_api_key", "exa_api_key", "apns_key_id", "apns_team_id", "apns_bundle_id", mode="before")
+    @field_validator("db_username", "db_password", "groq_api_key", "openai_api_key", "trip_advisor_api_key", "google_api_key", "tavily_api_key", "exa_api_key", mode="before")
     @classmethod
     def load_secrets(cls, v, info):
         if info.data.get("environment") == "production":
@@ -46,12 +41,6 @@ class Settings(BaseSettings):
                     v = secrets.get_db_credentials()['username']
                 elif info.field_name == "db_password":
                     v = secrets.get_db_credentials()['password']
-                elif info.field_name == "apns_key_id":
-                    v = secrets.get_apns_credentials()['key_id']
-                elif info.field_name == "apns_team_id":
-                    v = secrets.get_apns_credentials()['team_id']
-                elif info.field_name == "apns_bundle_id":
-                    v = secrets.get_apns_credentials()['bundle_id']
                 return v
             except Exception as e:
                 # If there's an error getting secrets, fall back to the env value
