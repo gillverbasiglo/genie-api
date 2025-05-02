57045bb16ca34b2c1187beb08df6f636136e804c
gillverbasiglo <dev.coder39@gmail.com>
2025-05-02 11:11:25 -0400

    Fix JSONB import
    Signed-off-by: carlos-genie <carlos@genietravel.ai>

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey, ARRAY, Boolean, Enum, JSONB
+from sqlalchemy import Column, String, DateTime, ForeignKey, ARRAY, Boolean, Enum
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from datetime import datetime, timezone
