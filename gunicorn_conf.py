b62ac1cccaaf6f13f78a590ba12da0f1ee6d4011
bilalzc <158590378+bilalzc@users.noreply.github.com>
2025-06-25 21:39:45 +0200

    reduced gunicorn workers

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 # Gunicorn config
 bind = "0.0.0.0:8000"  # Match this port in your ALB target group
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
 loglevel = "info"
 accesslog = "/var/log/gunicorn/access.log"
