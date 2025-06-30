687b42bf3c91a1ba010c02933b01114eee34197d
bilalzc <158590378+bilalzc@users.noreply.github.com>
2025-06-30 18:26:24 +0200

    add logging

--- app/database.py
+++ app/database.py
@@ -2,9 +2,18 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 # PostgreSQL connection string
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.db_username}:{settings.db_password.get_secret_value()}@{settings.host}:{settings.port}/{settings.database}"
+
+# Log the connection string (mask password for safety)
+masked_url = SQLALCHEMY_DATABASE_URL.replace(
+    settings.db_password.get_secret_value(), "*****"
+)
+logger.info(f"🔧 SQLAlchemy DB URL: {masked_url}")
 
 # Create an async engine
 engine = create_async_engine(
